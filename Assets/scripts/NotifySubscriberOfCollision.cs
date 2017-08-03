80117de5a9e81a21399c0ecfd46823c05c319c0e
Andrew Schools <andrewschools@me.com>
2017-08-03 17:46:00 -0400

    Some more work on Level 2.  Added a behavior that can notify subscribers when a collision occurs.  Created some new prefabs.

---
+++ Assets/scripts/NotifySubscriberOfCollision.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class NotifySubscriberOfCollision : MonoBehaviour {
+    public UnityEvent[] e;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        foreach (var sub in e) {
+            sub.Invoke();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        foreach (var sub in e) {
+            sub.Invoke();
+        }
+    }
+}
