5838c0d99697c2e9dd9b64d65c52db003181adea
Andrew Schools <andrewschools@me.com>
2017-08-04 12:59:03 -0400

    Adding back code that was previously removed, but is needed.

--- Assets/scripts/NotifySubscribersWhenApproaching.cs
+++ Assets/scripts/NotifySubscribersWhenApproaching.cs
@@ -6,13 +6,14 @@ using UnityEngine.Events;
 // Notify subscribers when specified GameObject approaches this GameObject
 // at distance specified.
 public class NotifySubscribersWhenApproaching : MonoBehaviour {
+    public GameObject other;
     public float distanceTrigger;
     protected bool triggered = false;
     public UnityEvent[] e;
 
     private void Update () {
         if (triggered == false) {
-            float currentDistance = Vector2.Distance(transform.position, transform.position);
+            float currentDistance = Vector2.Distance(other.transform.position, transform.position);
 
             if (currentDistance <= distanceTrigger) {
                 triggered = true;
