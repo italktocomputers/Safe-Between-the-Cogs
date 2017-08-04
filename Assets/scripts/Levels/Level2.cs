8c37a8d357fda6a2132ebf83d972dc9e40594161
Andrew Schools <andrewschools@me.com>
2017-08-04 16:34:29 -0400

    Added new behavior that can notify subscribers on interval.  Added more GameObjects to Level 2.

--- Assets/scripts/Levels/Level2.cs
+++ Assets/scripts/Levels/Level2.cs
@@ -6,11 +6,24 @@ using System;
 public class Level2 : Level {
     public GameObject gear1;
     public GameObject gear2;
+    public GameObject gear3;
+    public GameObject gear4;
+    public GameObject gear5;
     public GameObject donut1;
     public GameObject switch1;
 
     public GameObject platform1;
     public GameObject platform2;
+
+    public GameObject hammer1;
+    public GameObject hammer2;
+    public GameObject hammer3;
+    public GameObject hammer4;
+    public GameObject hammer5;
+    public GameObject hammer6;
+    public GameObject hammer7;
+    public GameObject hammer8;
+    public GameObject hammer9;
 
     override public void init() {
 
@@ -81,6 +94,56 @@ public class Level2 : Level {
         else {
             rotate.direction = "left";
             renderer.sprite = on;
+        }
+    }
+
+    public void toggleGear5Rotation() {
+        Rotate rotate = gear5.GetComponent<Rotate>();
+
+        if (rotate.direction == "left") {
+            rotate.direction = "right";
+        }
+        else {
+            rotate.direction = "left";
+        }
+    }
+
+    public void toggleHammer7Speed() {
+        Oscolate oscolate = hammer7.GetComponent<Oscolate>();
+
+        if (oscolate.speed1 == 2.0f) {
+            oscolate.speed1 = 3.0f;
+            oscolate.speed2 = 3.0f;
+        }
+        else {
+            oscolate.speed1 = 2.0f;
+            oscolate.speed2 = 2.0f;
+        }
+    }
+
+    public void toggleHammer8Speed() {
+        Oscolate oscolate = hammer8.GetComponent<Oscolate>();
+
+        if (oscolate.speed1 == 2.0f) {
+            oscolate.speed1 = 4.0f;
+            oscolate.speed2 = 4.0f;
+        }
+        else {
+            oscolate.speed1 = 2.0f;
+            oscolate.speed2 = 2.0f;
+        }
+    }
+
+    public void toggleHammer9Speed() {
+        Oscolate oscolate = hammer9.GetComponent<Oscolate>();
+
+        if (oscolate.speed1 == 2.0f) {
+            oscolate.speed1 = 5.0f;
+            oscolate.speed2 = 5.0f;
+        }
+        else {
+            oscolate.speed1 = 2.0f;
+            oscolate.speed2 = 2.0f;
         }
     }
 }
