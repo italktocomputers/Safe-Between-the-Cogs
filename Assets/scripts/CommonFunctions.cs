53567e963544557beba48b8ab1ace0edab2332fe
Andrew Schools <andrewschools@me.com>
2017-08-04 20:49:28 -0400

    Added delay to NotifySubscriberOfCollision which allowed me to remove AddDynamicBodyTypeToRBAfterCollision.cs.

--- Assets/scripts/CommonFunctions.cs
+++ Assets/scripts/CommonFunctions.cs
@@ -3,9 +3,14 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public static class CommonFunctions {
-    public static void exitGame() {
+public class CommonFunctions: MonoBehaviour {
+    public void exitGame() {
         SceneManager.LoadScene(0);
+    }
+
+    public void AddDynamicBodyTypeToRigidBody(GameObject gameObject) {
+        Rigidbody2D rb = gameObject.GetComponent<Rigidbody2D>();
+        rb.bodyType = RigidbodyType2D.Dynamic;
     }
 }
 
