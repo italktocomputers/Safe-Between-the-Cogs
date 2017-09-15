4523c683aafb5706543da44ff0da489ecd9e241c
Andrew Schools <andrewschools@me.com>
2017-09-15 18:21:40 -0400

    i was never incremented.

--- Assets/scripts/Leaderboard/Leaderboard_Local.cs
+++ Assets/scripts/Leaderboard/Leaderboard_Local.cs
@@ -78,6 +78,8 @@ public class Leaderboard_Local : ILeaderboard {
                 if (i > 24) {
                     leaderboard.Remove(record.Key);
                 }
+
+                i++;
             }
         }
     }
