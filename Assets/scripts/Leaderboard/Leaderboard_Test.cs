96533b82f810593d7709abd090e5c820136b1e16
Andrew Schools <andrewschools@me.com>
2017-09-14 21:54:57 -0400

    - Adding some more test data for Leaderboard - Welcome scene was sending wrong leaderboard index.

--- Assets/scripts/Leaderboard/Leaderboard_Test.cs
+++ Assets/scripts/Leaderboard/Leaderboard_Test.cs
@@ -5,16 +5,34 @@ public class Leaderboard_Test : ILeaderboard {
 	public Dictionary<string, float> getLeaderboard(int level, int limit, int offset) {
         Dictionary<string, float> results = new Dictionary<string, float>();
 
-        results["User 0"] = 300.80f;
-        results["User 1"] = 301.09f;
-        results["User 2"] = 303.41f;
-        results["User 3"] = 307.09f;
-        results["User 4"] = 307.57f;
-        results["User 5"] = 307.81f;
-        results["User 6"] = 308.06f;
-        results["User 7"] = 308.98f;
-        results["User 8"] = 309.03f;
-        results["User 9"] = 309.79f;
+        if (level == 1) {
+            results["Apple Computers"] = 300.80f;
+            results["Google"] = 301.09f;
+            results["Sun Microsystems"] = 303.41f;
+            results["Oracle"] = 307.09f;
+            results["Micro$oft"] = 307.57f;
+            results["Red Hat"] = 307.81f;
+            results["Hewlett-Packard"] = 308.06f;
+            results["IBM"] = 308.98f;
+            results["Intel"] = 309.03f;
+            results["Lenovo"] = 309.79f;
+            results["Dell"] = 310.09f;
+            results["Compaq"] = 310.88f;
+            results["Cisco Systems"] = 311.04f;
+            results["Amazon"] = 315.98f;
+        }
+        else {
+            results["Bill Gates"] = 301.09f;
+            results["Steve Jobs"] = 303.41f;
+            results["Denise Ritchie"] = 307.09f;
+            results["Ken Thompson"] = 307.57f;
+            results["Brian Kernighan"] = 307.81f;
+            results["Linus Torvalds"] = 308.06f;
+            results["Rob Pike"] = 308.98f;
+            results["Richard Stallman"] = 309.03f;
+            results["Guido van Rossum"] = 309.79f;
+            results["Bjarne Stroustrup"] = 310.09f;
+        }
 
         return results;
     }
